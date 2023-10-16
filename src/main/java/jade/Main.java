d2446a5ee082eaaab14afe8a81763b18b26c82e1
Varo L <stefanm.jay@gmail.com>
2023-10-15 23:14:47 -0700

    added comment

--- src/main/java/jade/Main.java
+++ src/main/java/jade/Main.java
@@ -1,5 +1,5 @@
 package jade;
-
+// After finishing making the engine work add controller and game pad listeners
 public class Main {
     public static void main(String[] args){
         Window window = Window.get();
