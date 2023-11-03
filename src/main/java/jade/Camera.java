58b72a6ed5f682bda4d5c7e106b3ff7d477eb4b7
Varoyaj <stefanm.jay@gmail.com>
2023-11-02 23:33:01 -0700

    Finish camera

--- src/main/java/jade/Camera.java
+++ src/main/java/jade/Camera.java
@@ -6,12 +6,13 @@ import org.joml.Vector3f;
 
 public class Camera {
     private Matrix4f projectionMatrix, viewMatrix;
-    private Vector2f position;
+    public Vector2f position;
 
     public Camera(Vector2f position){
         this.position = position;
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
+        adjustProjection();
     }
 
     public void adjustProjection(){
@@ -21,5 +22,15 @@ public class Camera {
 
     public Matrix4f getViewMatrix(){
         Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
+        Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
+        this.viewMatrix.identity();
+        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
+                                            cameraFront.add(position.x, position.y, 0.0f),
+                                            cameraUp);
+        return this.viewMatrix;
+    }
+
+    public Matrix4f getProjectionMatrix(){
+        return this.projectionMatrix;
     }
 }
