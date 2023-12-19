8f1285e79de15be585dfb7f205d17bc89d80acb5
Varoyaj <stefanm.jay@gmail.com>
2023-12-18 16:15:38 -0800

    Finished texture batching

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -2,15 +2,21 @@
 #version 330 core
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
+layout (location=2) in vec2 aTexCoords;
+layout (location=3) in float aTexid;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 
 out vec4 fColor;
+out vec2 fTexCoords;
+out float fTexid;
 
 void main()
 {
     fColor = aColor;
+    fTexCoords = aTexCoords;
+    fTexid = aTexid;
     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
@@ -18,10 +24,22 @@ void main()
 #version 330 core
 
 in vec4 fColor;
+in vec2 fTexCoords;
+in float fTexid;
+
+uniform sampler2D uTextures[8];
 
 out vec4 color;
 
 void main()
 {
-    color = fColor;
+    if (fTexid > 0){
+        int id = int(fTexid);
+        color = fColor * texture(uTextures[id], fTexCoords);
+        //color = vec4(fTexCoords, 0, 1);
+    }
+    else {
+        color = fColor;
+    }
+
 }
