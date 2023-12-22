77d0887e813c2b9d8f60af76a49b33da7aff3ebb
Varoyaj <stefanm.jay@gmail.com>
2023-12-21 23:50:22 -0800

    Finished Alpha blending and z-indexing

--- src/main/java/components/Spritesheet.java
+++ src/main/java/components/Spritesheet.java
@@ -7,16 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Spritesheet {
+
     private Texture texture;
     private List<Sprite> sprites;
 
-    public Spritesheet(Texture texture, int spriteWidth, int spriteHeight, int numSprites, int spacing){
+    public Spritesheet(Texture texture, int spriteWidth, int spriteHeight, int numSprites, int spacing) {
         this.sprites = new ArrayList<>();
 
         this.texture = texture;
         int currentX = 0;
         int currentY = texture.getHeight() - spriteHeight;
-        for (int i = 0; i < numSprites; i++){
+        for (int i=0; i < numSprites; i++) {
             float topY = (currentY + spriteHeight) / (float)texture.getHeight();
             float rightX = (currentX + spriteWidth) / (float)texture.getWidth();
             float leftX = currentX / (float)texture.getWidth();
@@ -32,14 +33,14 @@ public class Spritesheet {
             this.sprites.add(sprite);
 
             currentX += spriteWidth + spacing;
-            if (currentX >= texture.getWidth()){
+            if (currentX >= texture.getWidth()) {
                 currentX = 0;
                 currentY -= spriteHeight + spacing;
             }
         }
     }
 
-    public Sprite getSprite(int index){
+    public Sprite getSprite(int index) {
         return this.sprites.get(index);
     }
 }
