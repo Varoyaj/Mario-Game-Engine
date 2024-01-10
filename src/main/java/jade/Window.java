de87006895115534d6b074f0900f42020a9a0625
Varoyaj <stefanm.jay@gmail.com>
2024-01-09 18:42:21 -0800

    Finished adding Debug Drawing Functions

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,6 +3,7 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import renderer.DebugDraw;
 import scenes.LevelEditorScene;
 import scenes.LevelScene;
 import scenes.Scene;
@@ -142,11 +143,13 @@ public class Window {
             // Poll events
             glfwPollEvents();
 
+            DebugDraw.beginFrame();
+
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
             if (dt >= 0) {
-//                System.out.println(dt);
+                DebugDraw.draw();
                 currentScene.update(dt);
             }
 
